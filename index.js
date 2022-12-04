1eef7ac0fd802fa0d98e29713f75046a2e08a52f
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2022-12-04 15:27:53 +0100

    remaining exe completed(movies task)

--- index.js
+++ index.js
@@ -268,10 +268,6 @@ let diceNumber = rollTheDices(5);
 console.log(diceNumber, "\n");
 const loopTime = 5;
 
-for (let i = 0; i < loopTime; i++) {
-  console.log(`Iteration is #${i}`);
-}
-
 /* EXERCISE 9
 
 Write a function called howManyDays which receives a date as a parameter and returns the number of days passed since that date.
@@ -279,13 +275,13 @@ Write a function called howManyDays which receives a date as a parameter and ret
 */
 console.log("EXERCISE 9 \n");
 function howManyDays(x) {
-  let day = x.getDate();
+  let day = Math.floor(new Date() - x) / (1000 * 3600 * 24);
 
   return day;
 }
 // let date = new Date().toLocaleDateString("de-DE");
 
-let numOfDay = howManyDays(new Date());
+let numOfDay = howManyDays(new Date(2022, 12, 02));
 console.log(numOfDay, "\n");
 
 /* EXERCISE 10
@@ -321,152 +317,7 @@ console.log(birthday, "\n");
 
 // NOTE: the movies array used in some exercises is defined at the end of this file
 
-/* EXERCISE 11
-
-Write a function called deleteProp which receives an object and a string as parameters,
-
-and returns the given object after deleting its property named as the given string.
-
-*/
-console.log("EXERCISE 11 \n");
-function deleteProp(object, str) {
-  let keys = Object.keys(object);
-  console.log(keys);
-  for (let i = 0; i < keys.length; i++) {
-    if (str == keys[i]) {
-      let data = keys[i];
-      console.log("match: ", keys[i]);
-    }
-  }
-  //   let properties;
-
-  //   console.log(object);
-  //   properties = object.hasOwnProperty(str);
-  //   console.log(properties);
-
-  //   if (str in object) {
-  //     console.log(object.str);
-  //     return true;
-  //   } else {
-  //     return object;
-  //   }
-}
-let mySelf = {
-  name: "Nikunj",
-  surname: "Dobariya",
-  age: "23",
-  marriedOrNot: true,
-};
-let deleteObjectProp = deleteProp(mySelf, "marriedOrNot");
-console.log(deleteObjectProp, "\n");
-
-/* EXERCISE 12
-
-Write a function called oldestMovie which finds the oldest movie in the provided movies array.
-
-*/
-
-/* EXERCISE 13
-
-Write a function called countMovies which returns the number of movies contained in the provided movies array.
-
-*/
-
-/* EXERCISE 14
-
-Write a function called onlyTheTitles which creates an array with just the titles of the movies contained in the provided movies array.
-
-*/
-
-/* EXERCISE 15
-
-Write a function called onlyInThisMillennium which returns only the movies produced in this millennium from the provided movies array.
-
-*/
-
-/* EXERCISE 16
-
-Write a function called getMovieById which receives an id as a parameter and returns the movie with the given id from the provided movies array.
-
-*/
-
-/* EXERCISE 17
-
-Write a function called sumAllTheYears which returns the sum of all the years in which the movies in the provided movies array have been produced.
-
-*/
-
-/* EXERCISE 18
-
-Write a function called searchByTitle which receives a string as a parameter and returns all the movies in the provided movies array which contain that string in the title.
-
-*/
-
-/* EXERCISE 19
-
-Write a function called searchAndDivide which receives a string as a parameter and returns an object;
-
-this object should contain an array called match, made by all the movies from the provided movies array which contain the given string in the title,
-
-and another array unmatch with all the remaining ones.
-
-*/
-
-/* EXERCISE 20
-
-Write a function called "removeIndex" which receives a number as a parameter and returns the provided movies array without the element in the given position.
-
-*/
-
-// [EXTRAS] JS Advanced
-
-/* EXERCISE 21
-
-Create a function called "halfTree" which receives a number as a parameter and builds an "*" half tree with the given height.
-
-Example:
-
-halfTree(3)
-
-*
-
-**
-
-***
-
-*/
-
-/* EXERCISE 22
-
-Create a function called "tree" which receives a number as a parameter and builds an "*" tree with the given height.
-
-Example:
-
-tree(3)
-
-*
-
-***
-
-*****
-
-*/
-
-/* EXERCISE 23
-
-Create a function called "isItPrime" that receives a number as a parameter and returns true if the given number is a prime number.
-
-*/
-
-/* WHEN YOU ARE FINISHED
-
-Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today’s Eduflow.
-
-*/
-
-/* This movies array is used throughout the exercises. You’re not supposed to alter it. */
-
-const movies = [
+let movies = [
   {
     Title: "The Lord of the Rings: The Fellowship of the Ring",
 
@@ -649,3 +500,457 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
   },
 ];
+
+/* EXERCISE 11
+
+Write a function called deleteProp which receives an object and a string as parameters,
+
+and returns the given object after deleting its property named as the given string.
+
+*/
+console.log("EXERCISE 11 \n");
+function deleteProp(object, str) {
+  delete object[str];
+  return object;
+}
+let mySelf = {
+  name: "Nikunj",
+  surname: "Dobariya",
+  age: "23",
+  marriedOrNot: true,
+};
+let deleteObjectProp = deleteProp(mySelf, "marriedOrNot");
+console.log(deleteObjectProp, "\n");
+
+/* EXERCISE 12
+
+Write a function called oldestMovie which finds the oldest movie in the provided movies array.
+
+*/
+
+console.log("EXERCISE 12 \n");
+let oldestYear = 9999;
+let oldestMovieName;
+function oldestMovie(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].Year < oldestYear) {
+      oldestYear = arr[i].Year;
+      oldestMovieName = arr[i];
+    }
+  }
+  return oldestMovieName;
+}
+
+console.log(oldestMovie(movies), "\n");
+
+/* EXERCISE 13
+
+Write a function called countMovies which returns the number of movies contained in the provided movies array.
+
+*/
+console.log("EXERCISE 13 \n");
+function countMovies(arr) {
+  return arr.length;
+}
+
+console.log(countMovies(movies), "\n");
+
+/* EXERCISE 14
+
+Write a function called onlyTheTitles which creates an array with just the titles of the movies contained in the provided movies array.
+
+*/
+console.log("EXERCISE 14 \n");
+let moviesTitle = [];
+let moviesTitleArr = [];
+function onlyTheTitles(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    moviesTitle.push(arr[i].Title);
+    moviesTitleArr = moviesTitle;
+  }
+  return moviesTitleArr;
+}
+console.log(onlyTheTitles(movies), "\n");
+/* EXERCISE 15
+
+Write a function called onlyInThisMillennium which returns only the movies produced in this millennium from the provided movies array.
+
+*/
+console.log("EXERCISE 15 \n");
+
+let millenniumYearMovies = [];
+function onlyInThisMillennium(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].Year >= 2001) {
+      millenniumYearMovies.push(arr[i]);
+    }
+  }
+  return millenniumYearMovies;
+}
+console.log(onlyInThisMillennium(movies), "\n");
+
+/* EXERCISE 16
+
+Write a function called getMovieById which receives an id as a parameter and returns the movie with the given id from the provided movies array.
+
+*/
+console.log("EXERCISE 16 \n");
+let matchIdMovie = [];
+function getMovieById(arr, id) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].imdbID == id) {
+      matchIdMovie.push(arr[i]);
+    }
+  }
+  return matchIdMovie;
+}
+console.log(getMovieById(movies, "tt0399295"), "\n");
+
+/* EXERCISE 17
+
+Write a function called sumAllTheYears which returns the sum of all the years in which the movies in the provided movies array have been produced.
+
+*/
+console.log("EXERCISE 17 \n");
+let sumYear = 0;
+function sumAllTheYears(arr) {
+  let sumYear = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sumYear = sumYear + parseInt(arr[i].Year);
+  }
+  return sumYear;
+}
+console.log(sumAllTheYears(movies), "\n");
+/* EXERCISE 18
+
+Write a function called searchByTitle which receives a string as a parameter and returns all the movies in the provided movies array which contain that string in the title.
+
+*/
+console.log("EXERCISE 18 \n");
+function searchByTitle(str) {
+  let titleMatchMovies = [];
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].Title === str) {
+      titleMatchMovies.push(movies[i]);
+    }
+  }
+  return titleMatchMovies;
+}
+console.log(searchByTitle("The Lord of the Rings"), "\n");
+/* EXERCISE 19
+
+Write a function called searchAndDivide which receives a string as a parameter and returns an object;
+
+this object should contain an array called match, made by all the movies from the provided movies array which contain the given string in the title,
+
+and another array unmatch with all the remaining ones.
+
+*/
+console.log("EXERCISE 19 \n");
+function searchAndDivide(arr, str) {
+  let moviesObject = { match: [], unmatch: [] };
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].Title === str) {
+      moviesObject.match.push(arr[i]);
+    } else {
+      moviesObject.unmatch.push(arr[i]);
+    }
+  }
+  return moviesObject;
+}
+console.log(searchAndDivide(movies, "The Lord of the Rings"), "\n");
+
+/* EXERCISE 20
+
+Write a function called "removeIndex" which receives a number as a parameter and returns the provided movies
+array without the element in the given position.
+
+*/
+console.log("EXERCISE 20 \n");
+function removeIndex(num) {
+  let withoutIndexMovie = [];
+  for (let i = 0; i < movies.length; i++) {
+    if (i !== num) {
+      withoutIndexMovie.push(movies[i]);
+    }
+  }
+  return withoutIndexMovie;
+}
+console.log(removeIndex(1), "\n");
+// [EXTRAS] JS Advanced
+
+/* EXERCISE 21
+
+Create a function called "halfTree" which receives a number as a parameter and builds an "*" half tree with the given height.
+
+Example:
+
+halfTree(3)
+
+*
+
+**
+
+***
+
+*/
+
+console.log("EXERCISE 21 \n");
+function halfTree(num) {
+  let heightTree = "";
+  for (i = 1; i <= num; i++) {
+    for (j = 1; j <= i; j++) {
+      heightTree = heightTree + "*";
+    }
+    heightTree = heightTree + "\n";
+  }
+  return heightTree;
+}
+console.log(halfTree(7), "\n");
+/* EXERCISE 22
+
+Create a function called "tree" which receives a number as a parameter and builds an "*" tree with the given height.
+
+Example:
+
+tree(3)
+
+*
+
+***
+
+*****
+
+*/
+console.log("EXERCISE 22 \n");
+function tree(num) {
+  let heightTree = "";
+  for (i = 1; i <= num; i++) {
+    for (j = 1; j <= i; j++) {
+      heightTree = heightTree + "*";
+    }
+    for (k = 2; k <= i; k++) {
+      heightTree = heightTree + "*";
+    }
+    heightTree = heightTree + "\n";
+  }
+  return heightTree;
+}
+console.log(tree(3), "\n");
+
+/* EXERCISE 23
+
+Create a function called "isItPrime" that receives a number as a parameter and returns true if the given number is a prime number.
+
+*/
+
+console.log("EXERCISE 23 \n");
+let isPrimeNumber = true;
+function isItPrime(num) {
+  if (num === 1) {
+    return "1 is neither prime nor composite";
+  } else {
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+        isPrimeNumber = false;
+        break;
+      } else {
+        isPrimeNumber = true;
+      }
+    }
+  }
+  return isPrimeNumber;
+}
+console.log(isItPrime(4), "\n");
+
+/* WHEN YOU ARE FINISHED
+
+Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today’s Eduflow.
+
+*/
+
+/* This movies array is used throughout the exercises. You’re not supposed to alter it. */
+
+// const movies = [
+//   {
+//     Title: "The Lord of the Rings: The Fellowship of the Ring",
+
+//     Year: "2001",
+
+//     imdbID: "tt0120737",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "The Lord of the Rings: The Return of the King",
+
+//     Year: "2003",
+
+//     imdbID: "tt0167260",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "The Lord of the Rings: The Two Towers",
+
+//     Year: "2002",
+
+//     imdbID: "tt0167261",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "Lord of War",
+
+//     Year: "2005",
+
+//     imdbID: "tt0399295",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "Lords of Dogtown",
+
+//     Year: "2005",
+
+//     imdbID: "tt0355702",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "The Lord of the Rings",
+
+//     Year: "1978",
+
+//     imdbID: "tt0077869",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "Lord of the Flies",
+
+//     Year: "1990",
+
+//     imdbID: "tt0100054",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "The Lords of Salem",
+
+//     Year: "2012",
+
+//     imdbID: "tt1731697",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+
+//     Year: "1984",
+
+//     imdbID: "tt0087365",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "Lord of the Flies",
+
+//     Year: "1963",
+
+//     imdbID: "tt0057261",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "The Avengers",
+
+//     Year: "2012",
+
+//     imdbID: "tt0848228",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "Avengers: Infinity War",
+
+//     Year: "2018",
+
+//     imdbID: "tt4154756",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "Avengers: Age of Ultron",
+
+//     Year: "2015",
+
+//     imdbID: "tt2395427",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+//   },
+
+//   {
+//     Title: "Avengers: Endgame",
+
+//     Year: "2019",
+
+//     imdbID: "tt4154796",
+
+//     Type: "movie",
+
+//     Poster:
+//       "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+//   },
+// ];
